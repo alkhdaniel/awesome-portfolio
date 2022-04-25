2f0818011a5ffd99b61210c59975161908740385
alkhdaniel <alkh.daniel@gmail.com>
2022-04-25 02:00:41 +0200

    name hover effect

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import styled from "styled-components"
 
 const AboutContainer = styled.div`
-	width: var(--mainWidth);
-	margin:auto;
+    width: var(--mainWidth);
+    margin:auto;
+    padding:var(--padding)
 `;
 const Title = styled.h1`
-	text-align:center;
-	font-family: 'Segoe ui', sans-serif;
-	color: var(--titleColor)
+    text-align:center;
+    font-family: 'Segoe ui', sans-serif;
+    color: var(--titleColor)
 `;
 const TextContainer = styled.div`
 `;
@@ -18,12 +19,12 @@ const TextContainer = styled.div`
 const Header = ({ name, industry }) => {
   return (
     <AboutContainer>
-    	<Title>About</Title>
-    	<TextContainer>
-    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet dapibus felis. Nunc faucibus purus vitae libero dignissim semper. Curabitur eu tellus sagittis, cursus turpis nec, pellentesque enim. Phasellus dui lectus, iaculis sed diam sit amet, elementum consequat metus. Phasellus malesuada porta odio in malesuada. Praesent mattis nisl non blandit cursus. Integer nec nisl sagittis, consectetur felis venenatis, tempus purus. Etiam ut fermentum ligula, nec consectetur lectus. Phasellus consequat hendrerit tristique. Aenean ac tincidunt orci, quis bibendum mi. Donec tincidunt lorem ac tempus ornare. Sed ullamcorper arcu nibh, a porttitor neque egestas nec. Cras tincidunt vehicula felis. Suspendisse a imperdiet lacus, sed aliquam ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
-			<p>Suspendisse sed odio nec risus faucibus aliquet. Etiam vitae sodales ante. Ut sit amet augue enim. Donec est mauris, vulputate lacinia gravida malesuada, efficitur sed lacus. Suspendisse ut urna turpis. Praesent eros arcu, mollis quis maximus non, fringilla non orci. Praesent vel dolor id magna sollicitudin fringilla. Sed faucibus neque tempor, bibendum metus pharetra, blandit ex.</p>
-    	</TextContainer>
+    <Title>About</Title>
+    <TextContainer>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet dapibus felis. Nunc faucibus purus vitae libero dignissim semper. Curabitur eu tellus sagittis, cursus turpis nec, pellentesque enim. Phasellus dui lectus, iaculis sed diam sit amet, elementum consequat metus. Phasellus malesuada porta odio in malesuada. Praesent mattis nisl non blandit cursus. Integer nec nisl sagittis, consectetur felis venenatis, tempus purus. Etiam ut fermentum ligula, nec consectetur lectus. Phasellus consequat hendrerit tristique. Aenean ac tincidunt orci, quis bibendum mi. Donec tincidunt lorem ac tempus ornare. Sed ullamcorper arcu nibh, a porttitor neque egestas nec. Cras tincidunt vehicula felis. Suspendisse a imperdiet lacus, sed aliquam ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
+    <p>Suspendisse sed odio nec risus faucibus aliquet. Etiam vitae sodales ante. Ut sit amet augue enim. Donec est mauris, vulputate lacinia gravida malesuada, efficitur sed lacus. Suspendisse ut urna turpis. Praesent eros arcu, mollis quis maximus non, fringilla non orci. Praesent vel dolor id magna sollicitudin fringilla. Sed faucibus neque tempor, bibendum metus pharetra, blandit ex.</p>
+    </TextContainer>
     </AboutContainer>
-  )
+    )
 }
 export default Header
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,7 @@ import StarrySky from "./starrySky"
 
 const HeaderContainer = styled.div`
 	display:flex;
+	position:relative;
 	justify-content: center;
 	background: linear-gradient(#1c2948 5%, #614973 30%, #c86496 60%, #f5bea5 80%, var(--background-color) 100%);
 	height:75vh;
@@ -14,29 +15,82 @@ const Title = styled.h1`
 	position:relative;
 	text-align:center;
 	font-family: 'Segoe ui', sans-serif;
-	color: var(--headerColor)
+	color: var(--headerColor);
+	z-index:3;
 `;
 const Name = styled.div`
-	font-size:2.5rem
+	& {
+		font-size:2.5rem;
+		z-index:3;
+	}
+	&:before {
+		content: "Daniel Al-Khrysat";
+		white-space: nowrap;
+		position: absolute;
+		color: transparent;
+		background-image: repeating-linear-gradient(
+		    45deg,
+		    transparent 0,
+		    transparent 2px,
+		    white 2px,
+		    white 4px
+		);
+		-webkit-background-clip: text;
+		text-align:center;
+		z-index:4;
+		top: 0px;
+		left: 0px;
+		transition: 1s;
+		z-index:-1;
+	}
+	&:after {
+		content: "Daniel Al-Khrysat";
+		white-space: nowrap;
+		position: absolute;
+		color: transparent;
+		background-image: repeating-linear-gradient(
+			135deg,
+			transparent 0,
+			transparent 2px,
+			white 2px,
+			white 4px
+		);
+		-webkit-background-clip: text;
+		top: 0px;
+		left: 0px;
+		transition: 1s;
+		visibility:hidden;
+	}
+
+	&:hover:before {
+		top: 8px;
+		left: 8px;
+	}
+
+	&:hover:after {
+		visibility:visible;
+		top: -8px;
+		left: -8px;
+	}
 `;
 
 
 
 const Industry = styled.div`
-	font-size:2rem
+	font-size:1.75rem
 `;
 
 
 
 const Header = ({ name, industry }) => {
-  return (
-    <HeaderContainer>
-    	<StarrySky />
-    	<Title>
-	    	<Name>{name}</Name>
-	    	<Industry>{industry}</Industry>
-	    </Title>
-    </HeaderContainer>
-  )
+	return (
+		<HeaderContainer>
+			<StarrySky />
+			<Title>
+				<Name>{name}</Name>
+				<Industry>{industry}</Industry>
+			</Title>
+		</HeaderContainer>
+	)
 }
 export default Header
