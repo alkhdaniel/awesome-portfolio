2a4e61c71c32bf5a2bdf1483abe7fc14ae9600fb
alkhdaniel <alkh.daniel@gmail.com>
2022-04-29 01:03:06 +0200

    add about fields

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -13,6 +13,30 @@ const AboutContainer = styled.div`
 const Title = styled.h2`
 `;
 const TextContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(1, 1fr);
+	grid-template-rows: 1fr;
+	grid-column-gap: 0px;
+	grid-row-gap: 0px;
+	@media only screen and (min-width: 800px) {
+		grid-template-columns: repeat(3, 1fr);
+	}
+`;
+const TextItem = styled.div`
+`;
+const TextItemTitle = styled.h3`
+	text-transform: uppercase;
+	letter-spacing:2px;
+	text-align:center;
+	font-weight:400;
+`;
+const Seperator = styled.div`
+	width: 64px;
+	height: 2px;
+	margin: 8px auto;
+	background: var(--accentColor);
+`;
+const TextItemDescription = styled.div`
 `;
 
 
@@ -22,7 +46,21 @@ const About = ({ text }) => {
     <AboutContainer>
     <Title>About</Title>
     <TextContainer>
-    {text}
+    	<TextItem>
+    		<TextItemTitle>Develop</TextItemTitle>
+    		<Seperator />
+    		<TextItemDescription>Test</TextItemDescription>
+    	</TextItem>
+    	<TextItem>
+    		<TextItemTitle>Explore</TextItemTitle>
+    		<Seperator />
+    		<TextItemDescription>Test</TextItemDescription>
+    	</TextItem>
+    	<TextItem>
+    		<TextItemTitle>Learn</TextItemTitle>
+    		<Seperator />
+    		<TextItemDescription>Test</TextItemDescription>
+    	</TextItem>
     </TextContainer>
     </AboutContainer>
     )
